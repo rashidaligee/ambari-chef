1c5204dd398bb79b73762316a27e34029119f4c0
Artem Ervits <generic13@gmail.com>
2016-07-15 12:09:15 -0400

    applied style fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,26 +4,27 @@
 #
 # Copyright (c) 2016 Artem Ervits, All Rights Reserved.
 
-include_recipe "apt"
-include_recipe "java"
-#include_recipe "mysql_connector::j"
+include_recipe 'apt'
+include_recipe 'java'
+# include_recipe 'mysql_connector::j'
 
 # dependencies
-%w{wget curl unzip tar python2.7 openssl libpq5 postgresql postgresql-9.1 postgresql-client-9.1 postgresql-client-common postgresql-common ssl-cert}.each do |pkg|
-   package pkg do
-   end
+%w(wget curl unzip tar python2.7 openssl libpq5 postgresql postgresql-9.1 postgresql-client-9.1 postgresql-client-common postgresql-common ssl-cert).each do |pkg|
+  package pkg do
+  end
 end
 
 apt_repository 'ambari' do
-#   only_if { node['smartsense-chef']['use_local_repo'] }
-   uri 'http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.2.2.0'
-   components ['main']
-   distribution 'Ambari'
-   action :add
-# requires Chef 12.9
-   keyserver "keyserver.ubuntu.com"
-   key "B9733A7A07513CAD"
+  # only_if { node['smartsense-chef']['use_local_repo'] }
+  uri 'http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.2.2.0'
+  components ['main']
+  distribution 'Ambari'
+  action :add
+
+  # requires Chef 12.9
+  keyserver 'keyserver.ubuntu.com'
+  key 'B9733A7A07513CAD'
 end
 
-include_recipe "ambari-chef::ambari_server_setup"
-include_recipe "ambari-chef::ambari_agent_setup"
+include_recipe 'ambari-chef::ambari_server_setup'
+include_recipe 'ambari-chef::ambari_agent_setup'
