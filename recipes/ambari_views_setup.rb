d0a78b8661c24fdc9171c877b487face3583f462
Artem Ervits <generic13@gmail.com>
2016-10-31 13:34:15 -0400

    added Pig view

--- recipes/ambari_views_setup.rb
+++ recipes/ambari_views_setup.rb
@@ -95,7 +95,7 @@ curl --user admin:admin -i -H 'X-Requested-By: ambari' -X POST http://localhost:
     EOH
 end
 
-# create an instance of Tez  View
+# create an instance of Tez View
 bash 'create instance of Tez view' do
   code <<-EOH
 curl --user admin:admin -i -H 'X-Requested-By: ambari' -X POST http://localhost:8080/api/v1/views/TEZ/versions/0.7.0.2.5.0.0-22/instances/TEZ_NEW_INSTANCE \
@@ -107,6 +107,33 @@ curl --user admin:admin -i -H 'X-Requested-By: ambari' -X POST http://localhost:
       "webhdfs.url" : "webhdfs://hacluster",
       "yarn.ats.url" : "http://u1202.ambari.apache.org:8188",
       "yarn.resourcemanager.url" : "u1202.ambari.apache.org:8088"
+      }
+    }
+}'
+    EOH
+end
+
+# create an instance of Pig View
+bash 'create instance of Pig view' do
+  code <<-EOH
+curl --user admin:admin -i -H 'X-Requested-By: ambari' -X POST http://localhost:8080/api/v1/views/PIG/versions/1.0.0/instances/PIG_NEW_INSTANCE \
+--data '{
+  "ViewInstanceInfo" : {
+      "description" : "Pig View",
+      "label" : "Pig View",
+      "properties" : {
+      "webhdfs.client.failover.proxy.provider" : "org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider",
+      "webhdfs.ha.namenode.http-address.nn1" : "u1201.ambari.apache.org:50070",
+      "webhdfs.ha.namenode.http-address.nn2" : "u1201.ambari.apache.org:50070",
+      "webhdfs.ha.namenode.https-address.nn1" : null,
+      "webhdfs.ha.namenode.https-address.nn2" : null,
+      "webhdfs.ha.namenode.rpc-address.nn1" : "u1201.ambari.apache.org:8020",
+      "webhdfs.ha.namenode.rpc-address.nn2" : "u1202.ambari.apache.org:8020",
+      "webhdfs.ha.namenodes.list" : "nn1,nn2",
+      "webhdfs.nameservices" : "hacluster",
+      "webhdfs.url" : "webhdfs://hacluster",
+      "webhcat.hostname" : "u1203.ambari.apache.org",
+      "webhcat.port" : "50111"
       }
     }
 }'
