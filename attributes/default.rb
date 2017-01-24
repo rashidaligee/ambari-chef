4ccecdb8c0a1a3dd23548e4f9e1a6228e5822c1c
Artem Ervits <generic13@gmail.com>
2017-01-24 15:35:17 -0500

    fixed ambari_views_url

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,7 +11,7 @@ default['ambari-chef']['java_home'] = "/usr/lib/jvm/java-#{node[:java][:jdk_vers
 default['ambari-chef']['ambari_repo'] = 'http://public-repo-1.hortonworks.com/ambari/ubuntu14/2.x/updates/2.4.2.0'
 default['ambari-chef']['use_local_repo'] = 'false'
 
-default['ambari-chef']['ambari_views_url'] = 'http://localhost:8080/api/v1/views'
+default['ambari-chef']['ambari_views_url'] = 'http://default-ubuntu-1404:8080/api/v1/views'
 
 default['ambari-chef']['webhdfs.client.failover.proxy.provider'] = 'org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider'
 default['ambari-chef']['webhdfs.ha.namenode.http-address.nn1'] = 'u1201.ambari.apache.org:50070'
